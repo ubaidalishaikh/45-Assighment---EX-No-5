99638cc7a7be250811f32fe7cb70ede675ab01db
ubaidalishaikh <161380272+ubaidalishaikh@users.noreply.github.com>
2024-03-19 01:53:56 +0500

    Add files via upload
    I've completed 5thAssighment 🎉 Governer Sindh IT Intiative Kamran Khan Tessori

---
+++ Q5.js
@@ -0,0 +1,5 @@
+"use strict";
+let quote = "A person who never made a mistake never tried anything new.";
+let famousPerson = "Albert Einstein";
+let message = `${famousPerson} once said, ${quote}`;
+console.log(message);
---
+++ Q5.ts
@@ -0,0 +1,5 @@
+let quote: string = "A person who never made a mistake never tried anything new.";
+let famousPerson: string = "Albert Einstein";
+let message = `${famousPerson} once said, ${quote}`;
+
+console.log(message);
